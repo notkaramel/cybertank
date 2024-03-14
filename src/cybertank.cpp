9f6060340c5c870c7eb3ea9eca99681c444f08a0
Antoine Phan <antoine.phan@mail.mcgill.ca>
2024-03-14 01:26:32 -0400

    Checkpoint: Turning is not working ;-;

--- src/cybertank.cpp
+++ src/cybertank.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 
 // 1 ultrasonic sensor
-#define usTrigger 3
+#define usTrigger 3 // Digital PWM
 #define usEcho 4
 
-// 4 IR sensors to detect the outer line
-#define ir1 2
-#define ir2 4
-#define ir3 5
-#define ir4 6
+// 4 IR sensors to detect the outer line, analog IN
+#define ir1 A0
+#define ir2 A1
+#define ir3 A2
+#define ir4 A3
 
 // Left motor
 #define ena1 6 // left speed, Digital PWM
@@ -34,6 +34,9 @@ void setupIR()
   pinMode(ir4, INPUT);
 }
 
+/**
+ * Setup the motors
+ */
 void setupMotors()
 {
   // Motors
@@ -46,6 +49,9 @@ void setupMotors()
   pinMode(in4, OUTPUT);
 }
 
+/**
+ * Setup the ultrasonic sensor
+ */
 void setupUltrasonic()
 {
   // Ultrasonic sensor
@@ -54,6 +60,9 @@ void setupUltrasonic()
   pinMode(usEcho, INPUT);
 }
 
+/**
+ * Ping the IR sensors
+ */
 void pingIR()
 {
   int sensorValue = digitalRead(ir1); // seeing black = 1, seeing white = 0
@@ -67,6 +76,9 @@ void pingIR()
   }
 }
 
+/**
+ * Ping the ultrasonic sensor
+ */
 void pingUS()
 {
   digitalWrite(usTrigger, LOW);
@@ -81,41 +93,170 @@ void pingUS()
 }
 
 /**
- * Run the motors at the given speed
- * @param leftSpeed: 0x00 to 0xFF
- * @param rightSpeed: 0x00 to 0xFF
-*/
-void runMotors(uint8_t leftSpeed, uint8_t rightSpeed)
-{
-  // Left motor
-  Serial.print("Running: Left: ");
-  Serial.print(leftSpeed);
-  analogWrite(ena1, leftSpeed);
-  digitalWrite(in1, 0x1); // Forward
-  digitalWrite(in2, 0x0);
-
-  // Right motor
-  Serial.print(", Right: ");
-  Serial.println(rightSpeed);
-  analogWrite(ena2, rightSpeed);
-  digitalWrite(in3, 0x1);
-  digitalWrite(in4, 0x0);
-}
-
+ * Stop the motors
+ */
 void stopMotors()
 {
   Serial.println("Stopping motors");
   // Left motor
-  analogWrite(ena1, 0x0);
-  digitalWrite(in1, 0x0);
-  digitalWrite(in2, 0x0);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
 
   // Right motor
-  analogWrite(ena2, 0x0);
-  digitalWrite(in3, 0x0);
-  digitalWrite(in4, 0x0);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
 }
 
+void leftMotorForward(int speed)
+{
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  analogWrite(ena1, speed);
+}
+
+void rightMotorForward(int speed)
+{
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  analogWrite(ena2, speed);
+}
+
+void leftMotorBackward(int speed)
+{
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  analogWrite(ena1, speed);
+}
+
+void rightMotorBackward(int speed)
+{
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  analogWrite(ena2, speed);
+}
+
+/**
+ * Run the motors at the given speed
+ * @param leftSpeed: 0x00 to 0xFF
+ * @param rightSpeed: 0x00 to 0xFF
+ */
+void motorsForward(int leftSpeed, int rightSpeed)
+{
+  // Left motor
+  Serial.print("Running forward: Left: ");
+  Serial.print(leftSpeed);
+  leftMotorForward(leftSpeed);
+
+  // Right motor
+  Serial.print(", Right: ");
+  Serial.println(rightSpeed);
+  rightMotorForward(rightSpeed);
+}
+
+void turnRight(int speed)
+{
+  Serial.println("Turning right");
+  // Left motor continues forward
+  leftMotorForward(speed);
+
+  // Right motor going backward
+  rightMotorBackward(speed);
+}
+
+void turnLeft(int speed)
+{
+  Serial.println("Turning left");
+  // Right motor continues forward
+  rightMotorForward(speed);
+
+  // Left motor going backward
+  leftMotorBackward(speed);
+}
+
+void testMotorSpeed()
+{
+  Serial.println("First loop");
+  for (uint16_t i = 0x20; i <= 0xFF; i += 0x10)
+  {
+    motorsForward(i, i);
+    delay(1000);
+  }
+
+  Serial.println("Second loop");
+  for (uint16_t j = 0xFF; j >= 0x20; j -= 0x10)
+  {
+    motorsForward(j, j);
+    delay(1000);
+  }
+  stopMotors();
+  delay(5000);
+}
+
+void testLeftMotions(int speed)
+{
+  Serial.println("Test left motions");
+  stopMotors();
+  delay(100);
+  leftMotorForward(speed);
+  delay(2000);
+  stopMotors();
+  delay(100);
+  leftMotorBackward(speed);
+  delay(2000);
+  stopMotors();
+  delay(100);
+}
+
+void logicTest()
+{
+  Serial.println("Logic test");
+  Serial.println("0 0 state");
+  stopMotors();
+  delay(1000);
+  Serial.println("0 1 state");
+  leftMotorForward(0x40);
+  rightMotorForward(0x40);
+  delay(2000);
+  stopMotors();
+  Serial.println("1 0 state");
+  leftMotorBackward(0x40);
+  rightMotorBackward(0x40);
+  delay(2000);
+  stopMotors();
+  Serial.println("1 1 state");
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, HIGH);
+  delay(2000);
+  stopMotors();
+
+  Serial.println("Turning right");
+  rightMotorBackward(0x40);
+  leftMotorForward(0x40);
+  delay(2000);
+}
+
+void testTurningLeft(int speed)
+{
+  Serial.println("Test turning left");
+  stopMotors();
+  delay(100);
+  motorsForward(speed, speed);
+  delay(2000);
+  stopMotors();
+  delay(100);
+  turnLeft(speed);
+  delay(3000);
+  stopMotors();
+  delay(100);
+  turnRight(speed);
+  delay(1000);
+}
+
+/**
+ * Main setup function
+ */
 void setup()
 {
   // put your setup code here, to run once:
@@ -126,22 +267,12 @@ void setup()
   Serial.println("CyberTank is ready!");
 }
 
+/**
+ * Main loop function
+ */
 void loop()
 {
-  // Black reflects less IR light, so sensor value will be lower
-  Serial.println("First loop");
-  for (uint16_t i = 0x20; i <= 0xFF; i += 0x10)
-  {
-    runMotors(i, i);
-    delay(1000);
-  }
-
-  Serial.println("Second loop");
-  for (uint16_t j = 0xFF; j >= 0x20; j -= 0x10)
-  {
-    runMotors(j, j);
-    delay(1000);
-  }
-  stopMotors();
-  delay(5000);
+  // testTurningLeft(40);
+  logicTest();
+  // testLeftMotions(40);
 }
